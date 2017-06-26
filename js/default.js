d528e8c9c186eb289f475345d6906fd818f4bfa1
1627009 <g1627009@st.t-kougei.ac.jp>
2017-06-26 17:03:03 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 for(var i = 0; i < 100; ++i)
 {
   var li = document.createElement('li');
-  li.textContent = i + '回目';
+  li.textContent = i + 1 + '回目';
   document.getElementById('list').appendChild(li);
 }
