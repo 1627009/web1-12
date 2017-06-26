af4ecbf45d9363c020f5bcea9aa7a9f2f0a772d8
1627009 <g1627009@st.t-kougei.ac.jp>
2017-06-26 16:59:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,6 @@
-document.getElementById('choice').textContent = new Date();
+for(var i = 0; i < 100; ++i)
+{
+  var li = document.createElement('li');
+  li.textContent = i + '回目';
+  document.getElementById('list').appendChild(li);
+}
